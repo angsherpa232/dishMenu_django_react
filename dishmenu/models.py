c353408bfc940ddd4ded61d1625fbc969597fea2
angsherpa232 <angsherpa232@gmail.com>
2019-12-08 22:35:16 +0100

    Test added for DishMenu model

--- dishmenu/models.py
+++ dishmenu/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class DishMenu(models.Model):
     name = models.CharField(max_length=50)
-    price = models.IntegerField() 
+    price = models.IntegerField(blank=True, null=True) 
 
     def __str__(self):
         return self.name
--- dishmenu/tests.py
+++ dishmenu/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import DishMenu
 
-# Create your tests here.
+class DishMenuModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        DishMenu.objects.create(name='Hendl')
+        DishMenu.objects.create(price='18')
+
+    def test_name_content(self):
+        dish = DishMenu.objects.get(id=1)
+        expected_object_name = f'{dish.name}'
+        self.assertEquals(expected_object_name, 'Hendl')
+
+    def test_description_content(self):
+        dish = DishMenu.objects.get(id=2)
+        expected_object_price = f'{dish.price}'
+        self.assertEquals(expected_object_price, '18')
